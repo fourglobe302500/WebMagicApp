0491972cc940677f5118b0403e789344995e9acd
fourglobe302500 <franciscomiguelolima@gmail.com>
2020-11-22 18:00:51 +0000

    Adding the UserModel

---
+++ MagicApp/Models/UserModel.cs
@@ -0,0 +1,22 @@
+﻿using MagicApp.Stores;
+using MagicApp.Stores.UserStore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MagicApp.Models
+{
+    public class UserModel
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public void ValidSubmit(IStore<UserState> user)
+        {
+            ((UserStore)user).ChangeState(Name, null);
+        }
+    }
+}
